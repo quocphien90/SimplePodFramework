7a4787d2e7f32cb78f7eb8f27284b8eb5ce320ff
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-12-07 14:36:28 +0700

    Remove file

--- SimplePodFramework/SimpleModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SimpleModel.swift
-//  SimplePodFramework
-//
-//  Created by Pham Anh Quoc Phien on 12/7/18.
-//  Copyright © 2018 Pham Anh Quoc Phien. All rights reserved.
-//
-
-import Foundation
