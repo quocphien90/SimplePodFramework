e7f5de02e5919d651ee55df186b459c5c9d8ca61
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-12-07 14:30:02 +0700

    Demo framework

---
+++ SimplePodFramework/SimplePodFramework/SimplePodFramework.h
@@ -0,0 +1,19 @@
+//
+//  SimplePodFramework.h
+//  SimplePodFramework
+//
+//  Created by Pham Anh Quoc Phien on 12/7/18.
+//  Copyright © 2018 Pham Anh Quoc Phien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimplePodFramework.
+FOUNDATION_EXPORT double SimplePodFrameworkVersionNumber;
+
+//! Project version string for SimplePodFramework.
+FOUNDATION_EXPORT const unsigned char SimplePodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimplePodFramework/PublicHeader.h>
+
+
